6131053e24f2ecdf890d823cd14f2536fa61fc7c
darrelhong <darrel.hong@gmail.com>
2021-03-14 14:41:29 +0800

    update mockup using data from db

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
 
+from flask_app.db import query_db
+
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
-def hello():
-    return render_template('index.html')
+def index():
+    data = query_db('SELECT * FROM bin')
+    return render_template('index.html', data=data)
