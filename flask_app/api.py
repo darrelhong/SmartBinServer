ee2d296390d5eb236370f79a0e3eaba535bf78a3
darrelhong <darrel.hong@gmail.com>
2021-03-19 00:00:38 +0800

    add endpoint to update db fill percent

--- flask_app/api.py
+++ flask_app/api.py
@@ -1,6 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import request, Blueprint, jsonify
 
-from flask_app.db import query_db
+from flask_app.db import query_db, get_db
+
+# 200cm for now, to update accordingly
+BIN_HEIGHT = 200
 
 # all routes are prefixed with /api
 api = Blueprint('api', __name__)
@@ -24,3 +27,23 @@ def get_bin(name):
     if data:
         return jsonify(dict(data))
     return 'Bin not found', 400
+
+
+@api.route('/bin/update', methods=['POST'])
+def update_bin():
+    data = request.get_json()
+    current = query_db('SELECT * FROM bin WHERE name = ?',
+                       [data['microbit_name']], True)
+
+    name = data['microbit_name']
+    fill_percent = int(data['distance'] / BIN_HEIGHT * 100)
+    cur = get_db().cursor()
+    if current:
+        cur.execute('UPDATE bin SET fill_percent = ? WHERE name = ?',
+                    [fill_percent, name])
+    else:
+        cur.execute('INSERT INTO bin (name, fill_percent) VALUES (?, ?)', [
+                    name, fill_percent])
+    get_db().commit()
+
+    return 'Success'
