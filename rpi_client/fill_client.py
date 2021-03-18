6ac507879f31128522253ec70a423e20e724ba55
darrelhong <darrel.hong@gmail.com>
2021-03-19 00:01:51 +0800

    ad python script listen to microbit serial output and make api call

---
+++ rpi_client/fill_client.py
@@ -0,0 +1,47 @@
+import argparse
+import serial
+import requests
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', help="Microbit serial port", type=str)
+    parser.add_argument(
+        "--apihost", default='http://localhost:5000',
+        help="Server hostname", type=str)
+    args = parser.parse_args()
+
+    # for testing only
+    # r = requests.post(
+    #     '{}/api/bin/update'.format(args.apihost), json={
+    #         'bin_name': 'alpha',
+    #         'microbit_name': 'vavet',
+    #         'distance': 150
+    #     })
+    # print(r.text)
+
+    try:
+        ser = serial.Serial(port=args.port, baudrate=115200)
+        print('Listening on {}...'.format(args.port))
+
+        while True:
+            msg = ser.readline().decode('utf-8').strip()
+
+            msg_arr = msg.split('_')
+            fill_info = {
+                'bin_name': msg_arr[0],
+                'microbit_name': msg_arr[1],
+                'distance': int(msg_arr[2])
+            }
+
+            requests.post(
+                '{}/api/bin/update'.format(args.apihost), data=fill_info)
+
+    except serial.SerialException as err:
+        print('SerialExceptionL {}'.format(err))
+    except KeyboardInterrupt:
+        print('Program Terminated')
+
+
+if __name__ == "__main__":
+    main()
