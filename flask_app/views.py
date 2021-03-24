5dc4b5b884326b8f3e25df40d8f63dfcdf18e430
darrelhong <darrel.hong@gmail.com>
2021-03-25 00:03:24 +0800

    very baseline bin details page reuse same component

--- flask_app/views.py
+++ flask_app/views.py
@@ -14,3 +14,14 @@ def index():
                     "NATURAL JOIN bin "
                     "GROUP BY bin_name")
     return render_template('index.html', data=data)
+
+
+@views.route('/bin/<name>')
+def bin_page(name):
+
+    data = query_db("SELECT *, "
+                    "max(time_updated) AS 'fill_updated [timestamp]'"
+                    "FROM fill_level "
+                    "NATURAL JOIN bin "
+                    "WHERE bin_name = ?", [name], True)
+    return render_template('/bin/index.html', data=data)
